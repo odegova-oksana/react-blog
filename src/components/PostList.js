ba7d17268c95772beebbad1a5df415094d17b37c
Oxana Odegova <oxyapple@yandex.ru>
2021-01-02 15:53:51 +0100

    end of blog app

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchPosts } from "../actions";
+import { fetchPosts, fetchPostsAndUsers } from "../actions";
 import UserHeader from './UserHeader';
 
 class PostList extends React.Component {
 
     componentDidMount() {
-        this.props.fetchPosts();
+        this.props.fetchPostsAndUsers();
     }
 
     renderList() {
@@ -35,7 +35,7 @@ const mapStateToProps = (state) => {
     return { posts: state.posts };
 }
 
-export default connect(mapStateToProps, { fetchPosts })(PostList);
+export default connect(mapStateToProps, { fetchPostsAndUsers })(PostList);
 
 
 
